d729efa1fe6fb717a949043d76a5375eadd0a5db
Shyamlal Yadav <rajesh.yadav122002@gmail.com>
2025-06-02 00:53:04 +0530

    t20
    t20

--- MedcareApp-main/src/main/java/com/example/MedcareApp/Entity/Appointment.java
+++ MedcareApp-main/src/main/java/com/example/MedcareApp/Entity/Appointment.java
@@ -1,6 +1,7 @@
 package com.example.MedcareApp.Entity;
 
 
+
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,8 +12,10 @@ import java.util.UUID;
 @Document(collection = "appointments")
 public class Appointment {
     @Id
-    private String id = UUID.randomUUID().toString();
-
+    private String id;
+    private String gender;
+    private  String mobileNo;
+    private int patientAge;
     private String patientName;
     private String doctor;
     private String date;
@@ -20,4 +23,6 @@ public class Appointment {
     private String reason;
     private String fee;
     private String AppointmentStatus;
+    private String patientEmailId;
+    private String patientAddress;
 }
